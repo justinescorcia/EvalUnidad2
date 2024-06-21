a32b2e377dcae522fd185a9e94307dba9a0d0068
Justin Martin Muñoz Escorcia <210659@utxicotepec.edu.mx>
2024-06-21 13:42:49 -0600

    modelo y metos para usuarios

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routes.persona import persona
+from routes.usuario import usuario
+
+
 
 app=FastAPI()
+app.include_router(usuario)
 app.include_router(persona)
 print("Bienvenido a mi aplicacion")
