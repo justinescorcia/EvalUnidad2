450c05ead320e785581ecbaf455cab64dc0307de
Justin Martin Muñoz Escorcia <210659@utxicotepec.edu.mx>
2024-07-05 13:31:13 -0600

    creacion de personas en el crud

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from routes.persona import persona
 from routes.users import user
+from routes.persons import person
 
 app = FastAPI()
-app.include_router(persona)
+app.include_router(person)
 app.include_router(user)
 
 
